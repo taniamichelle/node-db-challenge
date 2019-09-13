58cb367aa33b75094996aabe9553340f83e934f1
tania <tanislavens@yahoo.com>
2019-09-13 13:44:08 -0400

    setting up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const projectsRouter = require('./projects/projects-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+
+module.exports = server;
